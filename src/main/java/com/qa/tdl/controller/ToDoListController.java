db6d337c08ddc1f478d4ced8e5b829574b483c0d
CGuthrieQA <CGuthrie@QA.com>
2021-02-11 15:39:57 +0000

    TDL-8 added READ (get all) method to ToDoListController

--- src/main/java/com/qa/tdl/controller/ToDoListController.java
+++ src/main/java/com/qa/tdl/controller/ToDoListController.java
@@ -1,9 +1,12 @@
 package com.qa.tdl.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,4 +32,11 @@ public class ToDoListController {
 		ToDoListDto created = this.service.create(toDoList);
 		return new ResponseEntity<>(created, HttpStatus.CREATED);
 	}
+	
+	// READ - get (all)
+	@GetMapping("/read")
+	public ResponseEntity<List<ToDoListDto>> readAll() {
+		return ResponseEntity.ok(this.service.readAll());
+	}
+	
 }
