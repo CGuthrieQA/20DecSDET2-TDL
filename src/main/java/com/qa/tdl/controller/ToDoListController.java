7801e80e14789bbd8a4fccf98cd1d6ed3d7e1143
CGuthrieQA <CGuthrie@QA.com>
2021-02-11 14:58:32 +0000

    TDL-6 added post method to ToDoListController

---
+++ src/main/java/com/qa/tdl/controller/ToDoListController.java
@@ -0,0 +1,32 @@
+package com.qa.tdl.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.qa.tdl.dto.ToDoListDto;
+import com.qa.tdl.persistance.domain.ToDoList;
+import com.qa.tdl.service.ToDoListService;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/todolist")
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class ToDoListController {
+
+	private ToDoListService service;
+	
+	// CREATE - post
+	@PostMapping("/create")
+	public ResponseEntity<ToDoListDto> create(@RequestBody ToDoList toDoList) {
+		ToDoListDto created = this.service.create(toDoList);
+		return new ResponseEntity<>(created, HttpStatus.CREATED);
+	}
+}
