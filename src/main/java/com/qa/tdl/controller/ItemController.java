bf80b402ab91c7d417b9326ffd4110069d6ce92f
CGuthrieQA <CGuthrie@QA.com>
2021-02-11 18:30:07 +0000

    TDL-14 added delete method to ItemController

--- src/main/java/com/qa/tdl/controller/ItemController.java
+++ src/main/java/com/qa/tdl/controller/ItemController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -45,5 +46,13 @@ public class ItemController {
 	public ResponseEntity<ItemDto> readById(@PathVariable Long id) {
 		return ResponseEntity.ok(this.service.readById(id));
 	}
-		
+	
+	// DELETE
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<ItemDto> delete(@PathVariable Long id) {
+		return this.service.delete(id) ?
+				new ResponseEntity<>(HttpStatus.NO_CONTENT)
+				: new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
 }
