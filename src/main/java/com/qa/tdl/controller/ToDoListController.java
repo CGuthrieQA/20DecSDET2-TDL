cfec6e483ba263d2a0fb5580a615c85dc5eb4069
CGuthrieQA <CGuthrie@QA.com>
2021-02-11 15:46:05 +0000

    TDL-9 added read by id method to ToDoListController

--- src/main/java/com/qa/tdl/controller/ToDoListController.java
+++ src/main/java/com/qa/tdl/controller/ToDoListController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,6 +38,12 @@ public class ToDoListController {
 	@GetMapping("/read")
 	public ResponseEntity<List<ToDoListDto>> readAll() {
 		return ResponseEntity.ok(this.service.readAll());
+	}
+	
+	// get one
+	@GetMapping("/read/{id}")
+	public ResponseEntity<ToDoListDto> readById(@PathVariable Long id) {
+		return ResponseEntity.ok(this.service.readById(id));
 	}
 	
 }
